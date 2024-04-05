a6b37586e0e063c74cfbcf8f7811333ec218694e
Lavanya1991827 <lavanya.chilukurthi@gmail.com>
2024-04-05 18:27:38 +0530

    shell script

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -3,9 +3,9 @@
 NUM1=$1
 NUM2=$2
 
-SUM=$(($NUM1+$NUM2))
+SUM=$($NUM1+$NUM2)
 
 echo "Total:: $SUM"
 echo "How many args passed:: $#"
-echo "All args passed:: #@"
+echo "All args passed:: $@"
 echo "Script name:: $0"
