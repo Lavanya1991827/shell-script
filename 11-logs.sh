b5742637aec4551b5609075ae3068d40154297bb
Lavanya1991827 <lavanya.chilukurthi@gmail.com>
2024-04-05 18:53:12 +0530

    shell script

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date  +%F-%H-%M-%)
+TIMESTAMP=$(date  +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "Script strarted executing at $TIMESTAMP" >> $LOGFILE
+echo "Script strarted executing at $TIMESTAMP" &>> $LOGFILE
 
 if [ $ID -ne 0 ]
 then
